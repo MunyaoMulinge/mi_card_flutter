3270f8a79698c3ae564256bc4e5b2d13d6a98cca
munyao <victor.mulinge@tangazoletu.com>
2025-09-22 08:41:28 +0300

    removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.cyanAccent,
         body: Container(),
       ),
     ),
