d240afce3bc261cbe4ce4022024ec6b22b60565b
munyao <victor.mulinge@tangazoletu.com>
2025-09-23 07:28:46 +0300

    single layout flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.cyanAccent,
-        body: Container(),
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 100.0,
+                width: 30.0,
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.deepOrange,
+                child: Text('New'),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.teal,
+                child: Text('Job'),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
